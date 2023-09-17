b612aaca8d8a51bae350c55f2a361f20d190999a
Berkay <berkaybilgen@yandex.com>
2023-09-17 13:33:45 +0300

    gitignore

---
+++ _next/static/ThA4FiyHExFp8R3aLLkas/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f33c0e3bcbb2e9c4.css","static/chunks/pages/index-a6a459e7a52d9486.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
