cd9c51d3dc6ce87b222b711f96e0aff289d2ae2f
Berkay <berkaybilgen@yandex.com>
2023-09-17 08:45:49 +0300

    Deploy settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  output: "export",
+  basePath: "/find-pairs",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
